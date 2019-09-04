90b744bed27d8555de08fa0f031bf08a48233a5c
jesugq <jesugq@gmail.com>
2019-09-04 18:22:35 -0500

    Add initial logic for obtaining emotions

--- emotions.py
+++ emotions.py
@@ -1,14 +1,39 @@
-import indicoio
-indicoio.config.api_key = '5bf59ad3450f583a8505fd13e44e8606'
+# Imports
+import csv
+import time
+import requests
 
-# single example
-first = indicoio.sentiment("I love writing code!")
+# Settings
+url = "'X-ApiKey: 5bf59ad3450f583a8505fd13e44e8606' 'https://apiv2.indico.io/sentiment' --data '{\"data\": \""
+lru = "\"}'"
 
-# batch example
-second = indicoio.sentiment([
-    "I love writing code!",
-    "Alexander and the Terrible, Horrible, No Good, Very Bad Day"
-])
+# Opening Files
+targetname = "./emotions.csv"
+readername = "./kaggle/Training.csv"
+targetfile = open(targetname, "w+")
+readerfile = open(readername, encoding="ISO-8859-1")
 
-print(first)
-print(second)
+# Table header creation
+csvtarget = csv.writer(targetfile)
+csvtarget.writerow(["emotion", "text", "score"])
+
+# Obtain Text
+csvreader = csv.reader(readerfile, delimiter=',')
+next(csvreader)
+csvtarget = csv.writer(targetfile)
+for row in csvreader:
+    text = row[0]
+    score = row[1]
+    payload = url + text + lru
+    print("\n\n")
+    print(payload)
+    print("\n\n")
+
+    response = requests.request("GET", payload)
+    data = response.json()
+
+    csvtarget.writerow([response, text, score])
+
+# Closing Files
+targetfile.close()
+readerfile.close()
