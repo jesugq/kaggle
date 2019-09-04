a667d57b0df67111bf88b7c1637b723c1719aac6
jesugq <jesugq@gmail.com>
2019-09-03 20:56:14 -0500

    Add reading capability from csv file

--- sentiments.py
+++ sentiments.py
@@ -8,27 +8,32 @@ url = "https://api.meaningcloud.com/sentiment-2.1"
 settings = "key=fa8df01f467649550a51ef4b9fd0221a&lang=en&txtf=plain&txt="
 headers = {'content-type': 'application/x-www-form-urlencoded'}
 
-# File Emptyness Verifier
-filename = "sentiments.csv"
-file = open(filename)
-opened = file.read(1)
-file.close()
+# Opening Files
+targetname = "./sentiments.csv"
+readername = "./kaggle/Training.csv"
+targetfile = open(targetname, "a+")
+readerfile = open(readername, encoding="ISO-8859-1")
 
-# Create Table Header
-file = csv.writer(open(filename, "a+"))
+# Table header Creation
+opened = targetfile.read(1)
 if not opened:
-    file.writerow(["model", "score_tag", "agreement", "subjectivity", "confidence", "irony"])
-file.close()
+    csvtarget = csv.writer(targetfile)
+    csvtarget.writerow(["model", "score_tag", "agreement", "subjectivity", "confidence", "irony"])
 
-# Text
-text = "horrible customer service hotel stay february 3rd 4th 2007my friend picked hotel monaco appealing website online package included champagne late checkout 3 free valet gift spa weekend 	friend checked room hours earlier came later 	pulled valet young man just stood 	asked valet open said 	pull bags didn__�_�_ offer help 	got garment bag suitcase came car key room number says not valet 	car park car street pull 	left key working asked valet park car gets 	went room fine bottle champagne oil lotion gift spa 	dressed went came got bed noticed blood drops pillows sheets pillows 	disgusted just unbelievable 	called desk sent somebody 20 minutes later 	swapped sheets left apologizing 	sunday morning called desk speak management sheets aggravated rude 	apparently no manager kind supervisor weekend wait monday morning 	young man spoke said cover food adding person changed sheets said fresh blood rude tone 	checkout 3pm package booked 	12 1:30 staff maids tried walk room opening door apologizing closing 	people called saying check 12 remind package 	finally packed things went downstairs check 	quickly signed paper took 	way took closer look room 	unfortunately covered food offered charged valet 	called desk ask charges lady answered snapped saying aware problem experienced monday like told earlier 	life treated like hotel 	not sure hotel constantly problems lucky ones stay recommend anybody know 	 "
-payload = "".join((settings, text))
+# Obtain Text
+csvreader = csv.reader(readerfile, delimiter=',')
+csvtarget = csv.writer(targetfile)
+for row in csvreader:
+    text = row[0]
+    payload = "".join((settings, text))
 
-# Request to JSON
-response = requests.request("POST", url, data=payload.encode('utf-8'), headers=headers)
-data = response.json()
+    # Request to JSON
+    response = requests.request("POST", url, data=payload.encode('utf-8'), headers=headers)
+    data = response.json()
 
-# Write to CSV File
-file = csv.writer(open(filename, "a+"))
-file.writerow([data["model"], data["score_tag"], data["agreement"], data["subjectivity"], data["confidence"], data["irony"]])
-file.close()
+    # Write to CSV File
+    csvtarget.writerow([data["model"], data["score_tag"], data["agreement"], data["subjectivity"], data["confidence"], data["irony"]])
+
+# Closing Files
+targetfile.close()
+readerfile.close()
