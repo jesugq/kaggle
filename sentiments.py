ba22f73a15050b4b60f76bf246d4e7af9bfbec29
jesugq <jesugq@gmail.com>
2019-09-20 19:55:28 -0500

    Rewrite sentiments.py for easier readability and better functionality

--- sentiments.py
+++ sentiments.py
@@ -1,52 +1,141 @@
+#####                               Settings                               #####
 # Imports
 import csv
 import json
 import time
 import requests
+import urllib.parse
 
-# Settings
-url = "https://api.meaningcloud.com/sentiment-2.1"
-settings = "key=fa8df01f467649550a51ef4b9fd0221a&lang=en&txtf=plain&txt="
-headers = {'content-type': 'application/x-www-form-urlencoded'}
+# Defining Protocol
+def defineProtocol():
+    url = "https://api.meaningcloud.com/sentiment-2.1"
+    headers = {'content-type': 'application/x-www-form-urlencoded'}
+
+    return url, headers
+
+# Defining Arguments
+def defineArguments():
+    key = "key=fa8df01f467649550a51ef4b9fd0221a"
+    lang = "lang=en"
+    txtf = "plain"
+    uw = "uw=y"
+    txt = "txt="
+    args = key +"&"+ lang +"&"+ txtf +"&"+ uw +"&"+ txt
+
+    return args
 
 # Opening Files
-targetname = "./sentiments.csv"
-readername = "./kaggle/Training.csv"
-targetfile = open(targetname, "w+")
-readerfile = open(readername, encoding="ISO-8859-1")
-
-# Table header Creation
-csvtarget = csv.writer(targetfile)
-csvtarget.writerow(["model", "score_tag", "agreement", "subjectivity", "confidence", "irony", "text", "score"])
-
-# Obtain Text
-csvreader = csv.reader(readerfile, delimiter=',')
-next(csvreader)
-csvtarget = csv.writer(targetfile)
-for row in csvreader:
-    # Obtain values from our file
-    text = row[0]
-    score = row[1]
-    payload = "".join((settings, text))
-
-    # Request to JSON
-    response = requests.request("POST", url, data=payload.encode('utf-8'), headers=headers)
-    data = response.json()
-
-    # Get Attributes Individually
-    model = data.get("model", "")
-    score_tag = data.get("score_tag", "")
-    agreement = data.get("agreement", "")
-    subjectivity = data.get("subjectivity", "")
-    confidence = data.get("confidence", "")
-    irony = data.get("irony", "")
-
-    # Write to CSV File
-    csvtarget.writerow([model, score_tag, agreement, subjectivity, confidence, irony, text, score])
+def openFiles():
+    readername = "../input/Training.csv"
+    writername = "../extracted/sentiments.csv"
+    readerfile = open(readername, encoding="ISO-8859-1")
+    writerfile = open(writername, "w+")
     
-    # MeaningCloud's Requests per Second Limit
-    time.sleep(2)
-    
+    return readerfile, writerfile
+
 # Closing Files
-targetfile.close()
-readerfile.close()
+def closeFiles(readerfile, writerfile):
+    readerfile.close()
+    writerfile.close()
+
+# Creating CSV Builders
+def createBuilders(readerfile, writerfile):
+    readercsv = csv.reader(readerfile, delimiter=',')
+    writercsv = csv.writer(writerfile)
+    
+    # Ignore readerfile's header
+    next(readercsv)
+    return readercsv, writercsv
+
+# Obtain position, text and rating
+def obtainValues(row):
+    text = row[0]
+    rating = row[1]
+
+    return text, rating
+
+# Call the Meaning Cloud API and return answer
+def httpSentiments(url, headers, payload):
+    response = requests.request("POST", url, data=payload, headers=headers)
+    json = response.json()
+
+    return response, json
+
+# Create the header of the csv result file
+def createHeader(writercsv):
+    writercsv.writerow(
+        [
+            "index"
+            "rating"
+            "model",
+            "score_tag",
+            "agreement",
+            "subjectivity",
+            "confidence",
+            "irony",
+        ]
+    )
+
+# Create each row of the csv result file
+def createRow(writercsv, index, rating, data):
+    writercsv.writerow(
+        [
+            index,
+            rating,
+            data["model"],
+            data["score_tag"],
+            data["agreement"],
+            data["subjectivity"],
+            data["confidence"],
+            data["irony"],
+        ]
+    )
+
+# Call each attribute individually
+def extractAttributes(json):
+    data = dict()
+
+    data["model"] = json.get("model")
+    data["score_tag"] = json.get("score_tag")
+    data["agreement"] = json.get("agreement")
+    data["subjectivity"] = json.get("subjectivity")
+    data["confidence"] = json.get("confidence")
+    data["irony"] = json.get("irony")
+
+    return data
+
+#####                                Logic                                #####
+# Get settings
+url, headers = defineProtocol()
+arguments = defineArguments()
+
+# Get files and builders
+readerfile, writerfile = openFiles()
+readercsv, writercsv = createBuilders(readerfile, writerfile)
+
+# Write the header
+createHeader(writercsv)
+
+# Run for every line in the readerfile
+index = 0
+for row in readercsv:
+    # Obtain the values from each row
+    text, rating = obtainValues(row)
+
+    # Parse for usage in http request
+    parsed = urllib.parse.quote(text)
+    payload = arguments + parsed
+
+    # Call http request
+    response, json = httpSentiments(url, headers, payload)
+    data = extractAttributes(json)
+
+    # Write the obtained value
+    createRow(writercsv, index, rating, data)
+
+    # Respect MeaningCloud's rate limit
+    index += 1
+    time.sleep(0.6)
+    
+# Close files
+closeFiles(readerfile, writerfile)
